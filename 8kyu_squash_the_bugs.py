f3b2f127620485b3da85135537df365190758c9b
choidslab <choids.lab@gmail.com>
2020-01-22 14:51:28 +0900

    codwars 8kyu 문제풀이

--- 8kyu_squash_the_bugs.py
+++ 8kyu_squash_the_bugs.py
@@ -1,15 +1,19 @@
 def find_longest(string):
     spl = string.split(" ")
-    print(spl)
     longest = 0
     i = 0
 
-    while (i > len(spl)):
-
-        if (len(spl(i)) > longest):
+    while (i < len(spl)):
+        if (len(spl[i]) >= longest):
             longest = len(spl[i])
+        i += 1
 
     return longest
 
 
-print(find_longest("Basic tests"))
+# print(find_longest("Basic tests"))
+print(find_longest("The quick white fox jumped around the massive dog"))
+print(find_longest("Take me to tinseltown with you"))
+print(find_longest("Sausage chops"))
+print(find_longest("Wind your body and wiggle your belly"))
+print(find_longest("Lets all go on holiday"))
